b944cb8e3697a0a3a998de2ed71e60ed4f619845
Jamie Bort <jamiebort@gmail.com>
2023-04-16 15:31:36 -0600

    Merge the Svelte/ directory in from the Svelte_Native branch.

---
+++ Svelte/attempt02/my-svelte-project/src/main.ts
@@ -0,0 +1,9 @@
+import './app.css'
+import App from './App.svelte'
+
+const app = new App({
+  // NOTE: the second pont after the entry point.
+  target: document.getElementById('app'),
+})
+
+export default app
