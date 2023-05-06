2917e788248f2856a20afb122d4a06539dfef43c
Jamie Bort <jamiebort@gmail.com>
2023-05-06 12:31:09 -0600

    Get a React Native app working on my phone.

--- ReactNative/attempt02/AwesomeProject/App.js
+++ ReactNative/attempt02/AwesomeProject/App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+// import {
+//   StyleSheet,
+//   Button,
+//   View,
+//   SafeAreaView,
+//   Text,
+//   Alert,
+// } from 'react-native';
+
+const onPressLearnMore = () => console.log("pressed");
+const onPressLearnMoreAgain = () => console.log("pressed again");
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={onPressLearnMore}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <Button
+        onPress={onPressLearnMoreAgain}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
